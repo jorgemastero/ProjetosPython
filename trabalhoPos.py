762321eac358f47b87ee378d47137c5f2ca630ae
Jorge Henrique <jorge_hjv@hotmail.com>
2024-10-07 22:50:16 -0300

    ProjetosPython

--- trabalhoPos.py
+++ trabalhoPos.py
@@ -1,110 +1,130 @@
-import nltk  # para remoção das stopwords
-import re  # para remoção dos números
-from nltk.corpus import stopwords  # para remoção das stopwords
-from nltk.stem import RSLPStemmer  # para Stemming
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
-import matplotlib.pyplot as plt
-from googletrans import Translator
+import pandas as pd
 import numpy as np
+import re
+import skfuzzy as fuzz
+import matplotlib.pyplot as plt
+import seaborn as sns
+import nltk
 from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer, WordNetLemmatizer
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import LatentDirichletAllocation
 from wordcloud import WordCloud
+import skfuzzy.control as ctrl
+from matplotlib.colors import LinearSegmentedColormap
+from collections import Counter
+import warnings
+from bs4 import BeautifulSoup
+import requests
 
-nltk.download('stopwords')
-nltk.download('rslp')
+# Suprimir o FutureWarning
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+# Conjunto de dados é fundamental para realizar tarefas de tokenização na sua análise de linguagem natural
+nltk.download('punkt') 
+nltk.download('stopwords') 
+#WordNet é como um dicionário eletrônico que contém informações sobre o significado das palavras em inglês, suas relações semânticas (sinônimos, antônimos, hiperônimos, hipônimos) e exemplos de uso.
+nltk.download('wordnet')  
 nltk.download('vader_lexicon')
 
-##referenciando o local do arquivo
+def site_de_busca():
+    print('Escolha onde você deseja realizar a busca: Amazon ou Mercado livre: ')
+    buscador_de_preco = input("")
+    if buscador_de_preco == "Amazon":
+        url = 'https://www.amazon.com.br/Fritadeira-Elétrica-Start-Elgin-Litros/dp/B0CFG1JZGY?th=1'
+        try:
+            requisicao = requests.get(url)
+            site = BeautifulSoup(requisicao.text,'html.parser')
 
-caminho = r"C:\\Users\\jorge\\Desktop\\documentos\\Avaliação de produto.txt"
+            #CAPTURAR INFORMAÇÕES: TÍTULO DA PÁGINA E PRINCIPAIS AVALIAÇÕES DO PRODUTO
 
-# Processo para tratar o texto
+            title = site.find('title')
+            review = site.find_all('div',class_='a-expander-content reviewText review-text-content a-expander-partial-collapse-content')
 
-with open(caminho, 'r', encoding='UTF-8') as arquivo:
-    texto = arquivo.read()
+            print(f'Análise para: {title.text}\n')
+            print('Principais Avaiações do produto:\n')
+            print('******************************************************************************************************************************************************')
+            cont = 1
+            avaliacoes = []
+            for i in review:
+                print(f'Review: {cont}')
+                print(i.text)
+                cont+=1
+                print('----------------------------------------------------------------------------------------------------------------------------------------------')
+                avaliacoes.append(i.text)
 
-translator = Translator()
-texto1 = texto
-traducao = translator.translate(texto1, src='pt', dest='en')
+        except Exception as e:
+            print(f"Erro: {e}")
+            return []
+    elif buscador_de_preco == "Mercado livre":
+        url = 'https://www.mercadolivre.com.br/fritadeira-air-fryer-eletrica-start-fry-35l-110v-elgin/p/MLB28328271?pdp_filters=item_id:MLB4237860714'
+        try:
+            requisicao = requests.get(url)
+            site = BeautifulSoup(requisicao.text,'html.parser')
 
-texto_traduzido = traducao.text
+            #CAPTURAR INFORMAÇÕES: TÍTULO DA PÁGINA E PRINCIPAIS AVALIAÇÕES DO PRODUTO
 
-# Removendo as stopwords
-def remover_stopwords(texto_traduzido):
-    stopwords_pt = set(stopwords.words('english')).union({"report", "assessment", "useful", "0", "1", "2", "3", "4", "5"})
-    palavras = texto_traduzido.split()
-    palavras_sem_stopwords = [palavra for palavra in palavras if palavra.lower() not in stopwords_pt and not re.match(r'^\d+﹩', palavra)]
-    return ' '.join(palavras_sem_stopwords)
+            title = site.find('title')
+            review = site.find_all('p',class_='ui-review-capability-comments__comment__content ui-review-capability-comments__comment__content')
 
-def stemming(texto_traduzido):
-    stemmer = RSLPStemmer()
-    palavras = texto_traduzido.split()
-    palavras_stemmed = [stemmer.stem(palavra) for palavra in palavras]
-    return ' '.join(palavras_stemmed)
+            print(f'Análise para: {title.text}\n')
+            print('Principais Avaiações do produto:\n')
+            print('******************************************************************************************************************************************************')
+            cont = 1
+            avaliacoes = []
+            for i in review:
+                print(f'Review: {cont}')
+                print(i.text)
+                cont+=1
+                print('----------------------------------------------------------------------------------------------------------------------------------------------')
+                avaliacoes.append(i.text)
 
-texto_sem_stopwords = remover_stopwords(texto_traduzido)
-texto_stemmed = stemming(texto_sem_stopwords)
-
-texto_tokenizado = word_tokenize(texto_stemmed)
-frases = texto_tokenizado
-
-# Definir as funções de pertinência fuzzy com ajuste para capturar melhor os extremos
-def pertinencia_positivo(x):
-    return np.maximum(0, x)  # Pertinência positiva quando a pontuação é acima de 0
-
-def pertinencia_negativo(x):
-    return np.maximum(0, -x)  # Pertinência negativa quando a pontuação é abaixo de 0
-
-def pertinencia_neutro(x):
-    return 1 - np.abs(x)  # Pertinência neutra quando a pontuação está próxima de 0
-
-# Inicializar o analizador de sentimento do VADER
-
-sia = SentimentIntensityAnalyzer()
-
-# Calcular a pontuação de sentimento para cada frase
-pontuacoes_sentimento = [sia.polarity_scores(frase)['compound'] for frase in frases]
-
-# Classificar o sentimento de cada frase
-sentimentos = []
-palavras_positivas = []
-palavras_negativas = []
-for i, pontuacao in enumerate(pontuacoes_sentimento):
-    positivo = pertinencia_positivo(pontuacao)
-    negativo = pertinencia_negativo(pontuacao)
-    neutro = pertinencia_neutro(pontuacao)
-
-    # Comparar os valores fuzzy e determinar o sentimento predominante
-    if positivo > negativo and positivo > neutro:
-        sentimentos.append("Positivo")
-        palavras_positivas.extend(frases[i].split())
-    elif negativo > positivo and negativo > neutro:
-        sentimentos.append("Negativo")
-        palavras_negativas.extend(frases[i].split())
+        except Exception as e:
+            print(f"Erro: {e}")
+            return []
     else:
-        sentimentos.append("Neutro")
+        return "Buscador incorreto, por favor digite novamente"
+    
+    return site_de_busca()
 
-# Exibir os sentimentos classificados
+resultado = site_de_busca()
 
-# Criar uma nuvem de palavras para as palavras positivas
-wordcloud_positivas = WordCloud(width=800, height=400, max_words=100).generate(' '.join(palavras_positivas))
-plt.figure(figsize=(10, 5))
-plt.imshow(wordcloud_positivas, interpolation='bilinear')
-plt.axis('off')
-plt.title('Palavras Positivas')
-plt.show()
+def clean_text(resultado):
+    if isinstance(resultado, str): # Testa para ver se o objeto texto é uma string
+        # Texto fica todo minúsculo
+        text = resultado.lower()
 
-# Criar uma nuvem de palavras para as palavras negativas
-wordcloud_negativas = WordCloud(width=800, height=400, max_words=100).generate(' '.join(palavras_negativas))
-plt.figure(figsize=(10, 5))
-plt.imshow(wordcloud_negativas, interpolation='bilinear')
-plt.axis('off')
-plt.title('Palavras Negativas')
-plt.show()
+        # Remove caracteres especiais
+        text = re.sub(r'[^\w\s]', '', resultado)
 
-# Criar um gráfico de barras para os sentimentos
-plt.figure(figsize=(10, 5))
-plt.bar(['Positivo', 'Negativo', 'Neutro'], [sentimentos.count('Positivo'), sentimentos.count('Negativo'), sentimentos.count('Neutro')])
-plt.xlabel('Sentimento')
-plt.ylabel('Frequência')
-plt.title('Distribuição dos Sentimentos')
-plt.show()
+        # Tokenizar o texto
+        tokens = word_tokenize(resultado)
+
+        # Removendo palavras irrelevantes
+        stop_words = set(stopwords.words('english'))
+        tokens = [words for word in tokens if word not in stop_words]
+
+        # Iniciando o stemmer(PorterStemmer) e o lematizer (WordNetLematizer)
+        stemmer = PorterStemmer()
+        lemmatizer = WordNetLemmatizer()
+
+        # Aplicando stemming para palavras que não precisam de lematização
+        processed_tokens = []
+        for word in tokens: 
+            # Lematiza se a palavra for um verbo (ou outra classe gramatical)
+            if lemmatizer.lemmatize(word) != word:
+                processed_tokens.append(lemmatizer.lemmatize(word))
+            else:
+                #se não tiver lemmatização, aplicar stemming
+                processed_tokens.append(stemmer.stem(word))
+
+        #retorna o texto processado
+        return ' '.join(processed_tokens) # o .join une todos os elementos de uma lista em uma unica string separado pelo elemento dado entre as aspas
+    return ' '
+
+def load_and_preprocess_data(resultado):
+    df = load_and_preprocess_data(resultado)
+    df.describe()
+    print(f"DataFrame carregado com {len(df)} avaliações.")
